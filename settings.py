b34a6c04875af90ce1becc9dfdfa09481a77dd88
Dmitriy Belkin <demetrius.belkin@gmail.com>
2021-04-27 18:20:09 +0200

    Добавил памятку в коммент по user-agent

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+# Список user-agent, если нужно будет поменять http://www.useragentstring.com/pages/useragentstring.php
 user_agent = {'User-agent': 'Mozilla/5.0'}
 sitemap_url = 'http://radio-qa.com/sitemap.xml'
 #sitemap_url = 'https://more.tv/sitemap.xml'
