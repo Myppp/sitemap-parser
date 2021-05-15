c208dab85744627bfc06c323c0615a4c0ac99d90
Dmitriy Belkin <demetrius.belkin@gmail.com>
2021-05-15 16:38:08 +0200

    Убран ненужный XML selector

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,23 +7,22 @@ from requests_xml import XMLSession
 def test_status_code_200():
     no_success_urls = []
     result = XMLSession().get(settings.sitemap_url).xml.search('<loc>{}</loc>')
-    if settings.xml_selector != 0:
-        second_req = XMLSession().get(result[settings.xml_selector][0])
-        search_urls = second_req.xml.search('<loc>{}</loc>')
-        counter = 0
-        for target_url in search_urls:
-            if counter == settings.urls_limit:
-                break
-            counter += 1
-            request = requests.Session()
-            response = request.get(target_url[0], headers=settings.user_agent)
-            status_code = response.status_code
-            if status_code != 200:
-                no_success_urls.append({"url": target_url[0], "status_code": status_code})
-                with open('no_success_urls.txt', 'w', encoding='utf-8') as f:
-                    for list_element in no_success_urls:
-                        value = "url: " + list_element["url"] + " status_code:" + str(list_element["status_code"])
-                        f.write(value)
+    second_req = XMLSession().get(result[settings.xml_selector][0])
+    search_urls = second_req.xml.search('<loc>{}</loc>')
+    counter = 0
+    for target_url in search_urls:
+        if counter == settings.urls_limit:
+            break
+        counter += 1
+        request = requests.Session()
+        response = request.get(target_url[0], headers=settings.user_agent)
+        status_code = response.status_code
+        if status_code != 200:
+            no_success_urls.append({"url": target_url[0], "status_code": status_code})
+            with open('no_success_urls.txt', 'w', encoding='utf-8') as f:
+                for list_element in no_success_urls:
+                    value = "url: " + list_element["url"] + " status_code:" + str(list_element["status_code"])
+                    f.write(value)
     assert status_code == 200
     return status_code
 
